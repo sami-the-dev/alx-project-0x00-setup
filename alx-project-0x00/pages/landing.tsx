89e34c836c15d3ce73c48327dbdfa6be103440db
Ahmed Sami <ahmedssami.dev@gmail.com>
2025-07-21 20:01:26 +0300

    Update Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,6 +7,8 @@ const Landing: React.FC = () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <Button title="Clik" styles="rounded-sm bg-red-500 px-5 py-2" />
+      <Button title="Clik" styles="rounded-lg bg-green-500 px-5 py-2" />
+      <Button title="Clik" styles="rounded-full bg-blue-500 px-5 py-2" />
     </div>
   );
 };
